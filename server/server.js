2b4206bea2acb1f723f0bfc95b681428c02202b9
abifitrird <fratnadewi@gmail.com>
2020-12-02 18:42:19 +0700

    prepare for deployment

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const port = 5001;
+const port = process.env.PORT || 5001;
 
 require("dotenv").config();
 
